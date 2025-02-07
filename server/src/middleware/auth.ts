ee149ca4c43691b73dd1968dd05ae1896f41e656
this-is-yosuke <yosuke.kibe@gmail.com>
2025-02-07 16:16:46 -0700

    Updated auth middleware
    Checks header, gets the secret key, verifies the JWT, and attaches user to the request

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,4 +7,29 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  
+  // get the authorization header
+  const authHeader = req.headers.authorization;
+
+  // Check if auth is present
+  if(authHeader){
+    const token = authHeader.split(' ')[1];
+
+  //Obtain secret key
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+  
+  // Verify the JWT Token
+  jwt.verify(token, secretKey, (err, user) => {
+    if(err){
+      return res.sendStatus(403);
+    }
+
+    // Attaching user to the request
+    req.user = user as JwtPayload;
+    return next();
+  })
+  }else{
+    // Unauthorized status if auth header is absent
+    res.sendStatus(401);
+  };
 };

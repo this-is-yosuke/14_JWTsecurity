84bfef53c5908f555d1175bf44d73b2bb1d6bcf2
this-is-yosuke <yosuke.kibe@gmail.com>
2025-02-09 11:42:50 -0700

    Updated auth util in client
    Filled out function bodies for getProfile(), loggedIn(), isTokenExpired(), getToken(), login(), and logout().

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,30 +1,52 @@
 import { JwtPayload, jwtDecode } from 'jwt-decode';
+import { UserData } from '../interfaces/UserData';
 
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    return jwtDecode<UserData>(this.getToken());
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try{
+      // decodes the provided token
+      const decoded = jwtDecode<JwtPayload>(token);
+      // Check the token's expiration property and compares it to current time
+      if(decoded?.exp && decoded?.exp < Date.now() / 1000){
+        // Returns true if expired
+        return true;
+      }
+    }catch (err) {
+      // Returns false if decoding fails
+      return false;
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem('id_token', idToken);
     // TODO: redirect to the home page
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem('id_token');
     // TODO: redirect to the login page
+    window.location.assign('/');
   }
 }
 

5e1db0f94029a5a821bd568b57820fa3692b7e58
this-is-yosuke <yosuke.kibe@gmail.com>
2025-02-11 00:45:13 -0700

    Updated server.ts
    Changed the PORT

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,29 +9,30 @@ import { sequelize } from './models/index.js';
 
 // Extra configuration for Render Deployment, using Neon
 // const { Pool } = require('pg');
-import { Pool } from 'pg';
+// import { Pool } from 'pg';
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false // Most cloud providers require SSL
-    }
-});
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false // Most cloud providers require SSL
+//     }
+// });
 
 // pool.connect()
 //     .then(() => console.log('✅ Connected to PostgreSQL'))
 //     .catch(err => console.error('❌ Database connection error:', err));
-try{
-  pool.connect()
-  .then(() => console.log('✅ Connected to PostgreSQL'))
-}catch (err){
-  console.error('❌ Database connection error:', err);
-}
+// try{
+//   pool.connect()
+//   .then(() => console.log('✅ Connected to PostgreSQL'))
+// }catch (err){
+//   console.error('❌ Database connection error:', err);
+// }
   
 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
+const PORT = process.env.DATABASE_URL || 3001;
 
 // Serves static files in the entire client's dist folder
 app.use(express.static('../client/dist'));
